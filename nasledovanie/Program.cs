febf0f0ab0c2e33541954d523872ae66b0835521
Sedjex <kathshem@gmail.com>
2022-05-24 17:19:55 +0300

    first commit

--- nasledovanie/Program.cs
+++ nasledovanie/Program.cs
@@ -1,12 +1,71 @@
 ﻿using System;
 
-namespace nasledovanie
+namespace Inheritance
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Knight knight = new Knight(100, 50, 50);
+            Barbarian barbarian = new Barbarian(100,1,20,2);
+
+            barbarian.getDamage(90);
+            knight.getDamage(110);
+
+            barbarian.showInfo();
+            knight.showInfo();
+            Console.ReadKey();
+        }
+    }
+
+    class Warrior
+    {
+        protected int Health;
+        protected int Armor;
+        protected int Damage;
+
+        public Warrior(int health, int armor, int damage)
+        {
+            Health = health;
+            Armor = armor;
+            Damage = damage;
+        }
+
+        public void getDamage(int damage)
+        {
+            Health -= damage - Armor;
+        }
+
+        public void showInfo()
+        {
+            Console.WriteLine(Health);
+        }
+    }
+
+    class Knight : Warrior
+    {
+        public Knight (int health, int armor, int damage) : base(health,armor,damage)
+        {
+
+        }
+        public void Pray()
+        {
+            Armor += 2;
+        }
+    }
+
+    class Barbarian : Warrior
+    {
+
+        public Barbarian(int health, int armor, int damage, int attackSpeed) : base(health, armor, damage * attackSpeed)
+        {
+
+        }
+
+        public void Waaghh()
+        {
+            Armor -= 2;
+            Health += 10;
         }
     }
 }
